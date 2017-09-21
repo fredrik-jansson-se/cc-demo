c18bd1735934e0911c2d568caed5940aea1f469d
Fredrik Jansson <frjansso@cisco.com>
2017-09-21 12:42:25 -0700

    Fixed create

--- create_service.py
+++ create_service.py
@@ -1,23 +1,55 @@
 #!/usr/bin/env python
 
 import os
+import urllib2
+import json
+import time
 
-payload = {
-    "cloud-interconnect:cloud-interconnect": {
+host = os.environ['nso_host']
+
+baseUrl = host + '/restconf/data'
+auth_handler = urllib2.HTTPBasicAuthHandler()
+auth_handler.add_password(realm='restconf',
+                          uri=baseUrl,
+                          user='admin',
+                          passwd='admin')
+
+opener = urllib2.build_opener(auth_handler, urllib2.HTTPHandler(debuglevel=1))
+urllib2.install_opener(opener)
+
+tpayload = """{{
+    "cloud-interconnect:cloud-interconnect": {{
         "name": "the-customer",
         "dc-esc": "escDMZ",
         "cloud-esc": "escSJ",
-        "cloud-router-public-ip": os.environ['cloud_public_ip'],
-        "cloud-router-private-ip": os.environ['cloud_router_private_ip'],
-        "cloud-vpc-private-network": os.environ['cloud_private_network'],
-        "dc-router-public-ip": os.environ['dc_public_ip'],
-        "dc-router-private-ip": os.environ['dc_private_ip'],
-        "dc-router-private-network": os.environ['dc_private_network']
-    }
-}
+        "cloud-router-public-ip": "{}",
+        "cloud-router-private-ip": "{}",
+        "cloud-vpc-private-network": "{}",
+        "dc-router-public-ip": "{}",
+        "dc-router-private-ip": "{}",
+        "dc-router-private-network": "{}"
+    }}
+}}""".format(os.environ['cloud_public_ip'],
+             os.environ['cloud_router_private_ip'],
+             os.environ['cloud_private_network'],
+             os.environ['dc_public_ip'],
+             os.environ['dc_private_ip'],
+             os.environ['dc_private_network'])
 
-print("I'm here")
-print(payload)
+PUTREQ = urllib2.Request(baseUrl + '/cloud-interconnect=the-customer',
+                         headers={'Content-Type': 'application/yang-data+json',
+                                  'Accept': 'application/yang-data+json'},
+                         data=tpayload)
+PUTREQ.get_method = lambda: 'PUT'
+res = opener.open(PUTREQ)
 
-# http://localhost:8080/restconf/data/cloud-interconnect=the-customer
-# PUT
+REQ = urllib2.Request(baseUrl+'/cloud-interconnect=the-customer/plan/'
+                      'component=self/state=ready/status',
+                      headers={"Accept": "application/yang-data+json"})
+while True:
+    TXT = urllib2.urlopen(REQ).read()
+    JDATA = json.loads(TXT)
+    print(JDATA)
+    if JDATA['cloud-interconnect:status'] == 'reached':
+        break
+    time.sleep(5)
